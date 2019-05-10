634a8e47d627910d00532820f2a86ddc0cbab75e
Cheranga Hatangala <cheranga@gmail.com>
2019-05-10 20:16:40 +1000

    Adding a sample service created using TopShelf

---
+++ SomeService/InvoiceService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SomeService
+{
+    public class InvoiceService
+    {
+        public void OnStart()
+        {
+            Console.WriteLine("Started!");
+            //return Console.Out.WriteLineAsync("Started!");
+        }
+
+        public void OnStop()
+        {
+            Console.WriteLine("Stopped!");
+            //return Console.Out.WriteLineAsync("Stopped!");
+        }
+    }
+}
---
+++ SomeService/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Topshelf;
+
+namespace SomeService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(configurator =>
+            {
+                configurator.SetServiceName("SomeService");
+                configurator.SetDisplayName("SomeService");
+                configurator.SetDescription("SomeService");
+
+                configurator.RunAsLocalSystem();
+
+                configurator.Service<InvoiceService>(serviceConfigurator =>
+                {
+                    serviceConfigurator.ConstructUsing(() => new InvoiceService());
+
+                    serviceConfigurator.WhenStarted((service, hostControl) =>
+                    {
+                        service.OnStart();
+                        return true;
+                    });
+                    serviceConfigurator.WhenStopped((service, hostControl) =>
+                    {
+                        service.OnStop();
+                        return true;
+                    });
+                });
+            });
+        }
+    }
+}
